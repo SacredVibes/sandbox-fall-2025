62785fb185ff19a39277419878e53dbbf64bb45b
SacredVibes <man2153618@maricopa.edu>
2025-09-02 20:14:14 -0700

    hello

--- src/main/java/org/example/sandbox/Hello.java
+++ src/main/java/org/example/sandbox/Hello.java
@@ -3,7 +3,7 @@ package org.example.sandbox;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Word!");
     }
 
 }
